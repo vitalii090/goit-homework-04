17916870ce16b72b026e43bb409808677c803d19
vitalii090 <pipskyi.v@gmail.com>
2023-06-28 15:42:44 +0300

    add

---
+++ hw_03.py
@@ -0,0 +1,40 @@
+import datetime
+
+users = [
+    {'name': 'Bill', 'birthday': datetime.date(2023, 7, 2)},
+    {'name': 'Jill', 'birthday': datetime.date(2023, 7, 3)},
+    {'name': 'Kim', 'birthday': datetime.date(2023, 7, 4)},
+    {'name': 'Jan', 'birthday': datetime.date(2023, 7, 7)},
+    {'name': 'Alex', 'birthday': datetime.date(2023, 7, 9)},
+    {'name': 'Ben', 'birthday': datetime.date(2023, 7, 12)}
+
+]
+
+def get_birthdays_per_week(users):
+    current_date = datetime.datetime.now().date()
+    current_weekday = current_date.weekday()
+    next_week_start = current_date + datetime.timedelta(days=(7 - current_weekday))
+    next_week_end = next_week_start + datetime.timedelta(days=6)
+
+    birthdays = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': [], 
+                 'Saturday': [], 'Sunday': []}
+
+    for user in users:
+        birthday = user['birthday']
+        if next_week_start <= birthday <= next_week_end:
+            birthday_weekday = birthday.weekday()
+
+            # Перевірка, чи день народження був у вихідні
+            if birthday_weekday >= 5:
+                # Привітання у понеділок наступного тижня
+                birthdays['Monday'].append(user['name'])
+            else:
+                # Привітання у відповідний день
+                birthday_name = birthday.strftime('%A')
+                birthdays[birthday_name].append(user['name'])
+
+    for weekday, user_list in birthdays.items():
+        if user_list:
+            print(f"{weekday}: {', '.join(user_list)}")
+
+get_birthdays_per_week(users)
